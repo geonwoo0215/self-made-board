df23817bac0f9efa50beee102713bca520b7fc2e
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-05-18 00:47:16 +0900

    initial commit

---
+++ src/main/java/hello/selfmadeboard/SelfmadeboardApplication.java
@@ -0,0 +1,13 @@
+package hello.selfmadeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfmadeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfmadeboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/selfmadeboard/SelfmadeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.selfmadeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfmadeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
