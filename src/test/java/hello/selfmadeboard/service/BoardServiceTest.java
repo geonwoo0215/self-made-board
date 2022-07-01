b1543481c9252400b81d724d9010c007c84f289a
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-01 23:50:28 +0900

    Service Test

--- src/test/java/hello/selfmadeboard/service/BoardServiceTest.java
+++ src/test/java/hello/selfmadeboard/service/BoardServiceTest.java
@@ -2,36 +2,62 @@ package hello.selfmadeboard.service;
 
 import hello.selfmadeboard.domain.Board;
 import hello.selfmadeboard.repository.BoardRepository;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BoardServiceTest {
 
-//    @Autowired
-//    BoardService boardService;
-//
-//    @Autowired
-//    BoardRepository boardRepository;
-//
-//
-//    @Test
-//    @Transactional
-//    void save() {
-//        Board board = Board.builder()
-//                .writer("LEE")
-//                .title("hi")
-//                .content("hello")
-//                .build();
-//
-//        Long id = boardService.save(board);
-//
-//        Assertions.assertEquals(board,boardRepository.findById(id));
-//    }
+    @Autowired
+    BoardService boardService;
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Test
+    @Transactional
+    void save() {
+        //given
+        Board board = Board.builder()
+                .title("hello")
+                .content("hi")
+                .writer("Lee")
+                .view(1)
+                .build();
+
+        //when
+        Long savedId = boardService.save(board);
+
+        //Then
+        Assertions.assertThat(board).isEqualTo(boardRepository.findById(savedId));
+    }
+
+    @Test
+    @Transactional
+    void delete() {
+        //given
+        Board board = Board.builder()
+                .title("hello")
+                .content("hi")
+                .writer("Lee")
+                .view(1)
+                .build();
+
+        //when
+        Long savedId = boardService.save(board);
+        boardService.deleteById(savedId);
+        Optional<Board> deleteUser = Optional.ofNullable(boardService.findBoard(savedId));
+
+        //Then
+        org.junit.jupiter.api.Assertions.assertFalse(deleteUser.isPresent());
+
+    }
 }
