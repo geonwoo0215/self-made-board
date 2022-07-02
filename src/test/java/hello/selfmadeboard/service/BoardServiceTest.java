16df41d2042cc34598347c6d65e28484137be4ec
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-03 05:35:55 +0900

    삭제, 검색, 수정, 조회수 Service 테스트

--- src/test/java/hello/selfmadeboard/service/BoardServiceTest.java
+++ src/test/java/hello/selfmadeboard/service/BoardServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,40 +25,109 @@ class BoardServiceTest {
 
     @Test
     @Transactional
-    void save() {
+    void 저장() {
         //given
         Board board = Board.builder()
                 .title("hello")
                 .content("hi")
                 .writer("Lee")
-                .view(1)
                 .build();
 
         //when
         Long savedId = boardService.save(board);
 
         //Then
-        Assertions.assertThat(board).isEqualTo(boardRepository.findById(savedId));
+        Assertions.assertThat(board).isEqualTo(boardService.findBoard(savedId));
     }
 
     @Test
     @Transactional
-    void delete() {
+    void 수정(){
         //given
         Board board = Board.builder()
                 .title("hello")
                 .content("hi")
                 .writer("Lee")
-                .view(1)
+                .build();
+
+        //when
+        Long id = boardService.save(board);
+        boardService.updateBoard(id,"Oh","kim","Hey");
+        Board updateBoard = boardService.findBoard(id);
+
+        //then
+        Assertions.assertThat(updateBoard.getTitle()).isEqualTo("Oh");
+        Assertions.assertThat(updateBoard.getWriter()).isEqualTo("kim");
+        Assertions.assertThat(updateBoard.getContent()).isEqualTo("Hey");
+
+    }
+
+
+    @Test
+    @Transactional
+    void 삭제() {
+        //given
+        Board board = Board.builder()
+                .title("hello")
+                .content("hi")
+                .writer("Lee")
                 .build();
 
         //when
         Long savedId = boardService.save(board);
         boardService.deleteById(savedId);
-        Optional<Board> deleteUser = Optional.ofNullable(boardService.findBoard(savedId));
+
 
         //Then
-        org.junit.jupiter.api.Assertions.assertFalse(deleteUser.isPresent());
+        org.junit.jupiter.api.Assertions.assertNull(boardService.findBoard(savedId));
+
+    }
+
+    @Test
+    @Transactional
+    void 조회수_증가() {
+
+        //given
+        Board board = Board.builder()
+                .title("hello")
+                .content("hi")
+                .writer("Lee")
+                .build();
+
+        //when
+        Long savedId = boardService.save(board);
+        boardService.updateView(savedId);
+
+        //Then
+        Assertions.assertThat(boardService.findBoard(savedId).getView()).isEqualTo(1);
+    }
+
+    @Test
+    @Transactional
+    void 검색(){
+
+        //given
+        Board board = Board.builder()
+                .title("Hello")
+                .writer("lee")
+                .content("hi")
+                .build();
+        Board board2 = Board.builder()
+                .title("GoodBye")
+                .writer("kim")
+                .content("how are you")
+                .build();
+
+        String searchTitle = "Hello";
+
+        //when
+        Long saveId = boardService.save(board);
+        Long saveId2 = boardService.save(board2);
+        List<Board> boards = boardService.searchByTitle(searchTitle);
+
+        //then
+        Assertions.assertThat(boards).contains(board);
+        Assertions.assertThat(boards).doesNotContain(board2);
 
     }
 }
