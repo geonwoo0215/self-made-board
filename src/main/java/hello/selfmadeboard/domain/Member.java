755dd92f6ab2d56d882f1267b1bd8a6cc6d9dfbc
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-05 23:54:41 +0900

    Member domain 시큐리티 미적용

--- src/main/java/hello/selfmadeboard/domain/Member.java
+++ src/main/java/hello/selfmadeboard/domain/Member.java
@@ -4,34 +4,58 @@ import lombok.*;
 
 import javax.persistence.*;
 
+//@Entity
+//@Getter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Member {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false,unique = true)
+//    private String email;
+//
+//    @Column(nullable = false)
+//    private String username;
+//
+//    @Column
+//    private String password;
+//
+//    @Enumerated(EnumType.STRING)
+//    private Role role;
+//
+//    @Builder
+//    public Member(Long id, String email, String username, String password, Role role) {
+//        this.id = id;
+//        this.email = email;
+//        this.username = username;
+//        this.password = password;
+//        this.role = role;
+//    }
+//}
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
     private Long id;
 
     @Column(nullable = false,unique = true)
-    private String email;
+    private String loginId;
 
     @Column(nullable = false)
-    private String username;
+    private String name;
 
-    @Column
+    @Column(nullable = false)
     private String password;
 
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
     @Builder
-    public Member(Long id, String email, String username, String password, Role role) {
-        this.id = id;
-        this.email = email;
-        this.username = username;
+    public Member(String loginId, String name, String password){
+        this.loginId = loginId;
+        this.name = name;
         this.password = password;
-        this.role = role;
     }
 }
+
