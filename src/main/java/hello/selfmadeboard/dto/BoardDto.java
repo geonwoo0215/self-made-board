252a6664aae31aa3dc5bec8e25d8ae4f84a8b0fb
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-07 05:08:54 +0900

    Dto 수정

--- src/main/java/hello/selfmadeboard/dto/BoardDto.java
+++ src/main/java/hello/selfmadeboard/dto/BoardDto.java
@@ -1,37 +1,40 @@
 package hello.selfmadeboard.dto;
 
 import hello.selfmadeboard.domain.Board;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@NoArgsConstructor
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardDto {
 
+
     private Long id;
+
+    @NotBlank
     private String title;
     private String writer;
     private String content;
-    private int view;
+
+    private Integer view;
 
     public Board toEntity(){
-        Board build = Board.builder()
-                .id(id)
+        Board board = Board.builder()
                 .writer(writer)
                 .title(title)
                 .content(content)
-                .view(view)
                 .build();
-        return build;
+        return board;
     }
 
     @Builder
-    public BoardDto(Long id, String title, String writer, String content, int view) {
+    public BoardDto(Long id, String title, String writer, String content, Integer view) {
         this.id = id;
         this.title = title;
         this.writer = writer;
         this.content = content;
         this.view = view;
     }
+
 }
