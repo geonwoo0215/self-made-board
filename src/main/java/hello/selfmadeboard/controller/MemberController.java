597061359a9d2cead229652e578808b283369df9
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-07-12 23:21:26 +0900

    인터셉터 연습

--- src/main/java/hello/selfmadeboard/controller/MemberController.java
+++ src/main/java/hello/selfmadeboard/controller/MemberController.java
@@ -5,7 +5,9 @@ import hello.selfmadeboard.controller.Form.LoginForm;
 import hello.selfmadeboard.controller.Form.MemberForm;
 import hello.selfmadeboard.domain.Member;
 import hello.selfmadeboard.service.MemberService;
+import hello.selfmadeboard.session.SessionManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -13,14 +15,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/member")
-    public String MemberForm(@ModelAttribute("memberDto") MemberForm memberForm) {
+    public String MemberForm(@ModelAttribute("memberForm") MemberForm memberForm) {
         return "member/memberForm";
     }
 
@@ -39,7 +48,7 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult){
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult,HttpServletRequest request){
 
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -52,9 +61,22 @@ public class MemberController {
             return "login/loginForm";
         }
 
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return "redirect:/";
 
     }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        if(session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+
+    }
 
 }
