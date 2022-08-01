37550294d3e83ccb8b803de04d72baa7d3ba3d50
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-02 04:15:12 +0900

    feat : write 메서드 추가

---
+++ src/main/java/hello/selfmadeboard/service/BoardService.java
@@ -0,0 +1,22 @@
+package hello.selfmadeboard.service;
+
+import hello.selfmadeboard.controller.form.BoardForm;
+import hello.selfmadeboard.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public void write(BoardForm boardForm) {
+        log.info("저장!");
+        boardRepository.save(boardForm.toBoard());
+    }
+
+
+}
