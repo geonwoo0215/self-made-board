c48db3b898c186343383aaeb5404e2262ebc8c59
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-03 03:05:52 +0900

    refactor(ExControllerAdvice) : 생략된부분 수정

--- src/main/java/hello/selfmadeboard/controllerAdvice/ExControllerAdvice.java
+++ src/main/java/hello/selfmadeboard/controllerAdvice/ExControllerAdvice.java
@@ -4,19 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> badRequestHandle(MethodArgumentNotValidException e) {
         log.error("exception!", e);
         Map<String, String> errors =new HashMap<>();
