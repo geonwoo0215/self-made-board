96f8929ff2a8117edf4d49fc648b54698e567e47
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-08 04:57:35 +0900

    feat : 생성한 exception 을 통한 exceptionHandle

--- src/main/java/hello/selfmadeboard/controllerAdvice/ExControllerAdvice.java
+++ src/main/java/hello/selfmadeboard/controllerAdvice/ExControllerAdvice.java
@@ -1,13 +1,13 @@
 package hello.selfmadeboard.controllerAdvice;
 
+import hello.selfmadeboard.controller.form.ErrorResponseForm;
+import hello.selfmadeboard.exception.BoardException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,12 +15,33 @@ public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> badRequestHandle(MethodArgumentNotValidException e) {
-        log.error("잘못 된 입력입니다!", e);
-        Map<String, String> errors = new HashMap<>();
+    public ErrorResponseForm badRequestHandle(MethodArgumentNotValidException e) {
+        log.error("잘못된 요청!");
+        ErrorResponseForm response = ErrorResponseForm.builder()
+                .code("400")
+                .message("잘못된 요청합니다.")
+                .build();
 
-        e.getBindingResult().getAllErrors().forEach(a -> errors.put(((FieldError) a).getField(), a.getDefaultMessage()));
-        return errors;
+        for (FieldError fieldError : e.getFieldErrors()) {
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return response;
     }
 
+
+
+    @ExceptionHandler(BoardException.class)
+    public ResponseEntity<ErrorResponseForm> boardException(BoardException e) {
+        log.error("찾을 수 없습니다!");
+
+        int statusCode = e.getStatusCode();
+
+        ErrorResponseForm response = ErrorResponseForm.builder()
+                .code(String.valueOf(statusCode))
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(statusCode).body(response);
+
+    }
 }
