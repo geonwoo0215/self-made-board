e386f8f3382ac4d9ad3fd95102b236a10b521116
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-08 04:58:14 +0900

    feat : exception status 반환

--- src/main/java/hello/selfmadeboard/exception/BoardNotFound.java
+++ src/main/java/hello/selfmadeboard/exception/BoardNotFound.java
@@ -1,6 +1,6 @@
 package hello.selfmadeboard.exception;
 
-public class BoardNotFound extends RuntimeException{
+public class BoardNotFound extends BoardException{
 
     private static final String MESSAGE = "존재하지 않는 글입니다.";
 
@@ -8,4 +8,8 @@ public class BoardNotFound extends RuntimeException{
         super(MESSAGE);
     }
 
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
 }
